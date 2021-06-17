555e839a26371b1684443626850794efce2c2750
BletaGashi <gashi.bleta1@gmail.com>
2021-06-17 20:01:54 +0200

    Sistemimi i fileve

--- MyTherapy2/app/src/main/java/com/example/mytherapy/MainActivity.java
+++ MyTherapy2/app/src/main/java/com/example/mytherapy/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnKyqu;
+EditText etUsername, etPassword;
+Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnKyqu=findViewById(R.id.btnKyqu);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin = findViewById(R.id.btnLogin);
     }
 }
