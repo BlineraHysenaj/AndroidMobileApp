1f7f8fd667419e053d52f72d1d3ec8fc54c3ffb6
BlineraHysenaj <blinera.hysenaj@gmail.com>
2021-06-18 18:27:05 +0200

    Krijimi i eventit qe shfaqet pas klikimit te Login Butonit

--- MyTherapy2/app/src/main/java/com/example/mytherapy/MainActivity.java
+++ MyTherapy2/app/src/main/java/com/example/mytherapy/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 EditText etUsername, etPassword;
 Button btnLogin;
 Button btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,13 @@ Button btnRegister;
         etUsername.setText("Blinera.Hysenaj");
         btnLogin.setBackgroundColor(Color.YELLOW);
         btnLogin.setTextColor(Color.BLACK);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Mire se vini ne aplikacionin tone!", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
