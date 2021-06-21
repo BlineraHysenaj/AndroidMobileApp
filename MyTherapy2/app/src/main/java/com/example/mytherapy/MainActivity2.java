476a4e4e42d99f2b09c1cd6b1ee731570f49f1bd
BletaGashi <gashi.bleta1@gmail.com>
2021-06-21 21:47:47 +0200

    Largimi i disa erroreve, Versioni paraprak

--- MyTherapy2/app/src/main/java/com/example/mytherapy/MainActivity2.java
+++ MyTherapy2/app/src/main/java/com/example/mytherapy/MainActivity2.java
@@ -1,6 +1,5 @@
 package com.example.mytherapy;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,21 +16,18 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import android.os.Bundle;
-
-public class Register extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
     EditText etUsername, etPassword, etConfirmPassword;
     Button btnRegister;
-    FirebaseAuth firebaseAuth;
+   FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main2);
 
-//        EditText etUsername, etPassword, etConfirmPassword;
-//        Button btnRegister = null;
-//        FirebaseAuth firebaseAuth;
-
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        etConfirmPassword = findViewById(R.id.etConfirmPassword);
 
         firebaseAuth.getInstance();
 
@@ -39,7 +35,7 @@ public class Register extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Register.this,"Mire se vini ne aplikacionin tone!", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity2.this,"Mire se vini ne aplikacionin tone!", Toast.LENGTH_LONG).show();
                 String email = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 firebaseAuth.createUserWithEmailAndPassword(email, password)
@@ -52,7 +48,7 @@ public class Register extends AppCompatActivity {
 
                         }).addOnFailureListener(new OnFailureListener(){
                     @Override
-                    public void onFailure(@NonNull Exception e){
+                            public void onFailure(@NonNull Exception e){
                         Log.d("This", e.getMessage());
                     }
                 });
@@ -70,5 +66,6 @@ public class Register extends AppCompatActivity {
 //                etUsername.setError(null);
 //                return true;
 //            }
-        }
+//        }
     }
+}
